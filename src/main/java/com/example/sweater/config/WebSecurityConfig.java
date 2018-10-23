123a5334400fac01d63f0bbfc82f319aec4defaf
Yevheniy Goncharenko <yevheniy.goncharenko@gmail.com>
2018-10-23 18:20:27 +0300

    UI with BootStrap

--- src/main/java/com/example/sweater/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweater/config/WebSecurityConfig.java
@@ -18,23 +18,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private UserSevice userSevice;
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .antMatchers("/", "/registration", "/static").permitAll()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .permitAll();
-    }
-
-    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userSevice)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/registration", "/static/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
     }
 }
--- src/main/resources/templates/greeting.ftl
+++ src/main/resources/templates/greeting.ftl
@@ -1,6 +1,8 @@
 <#import "parts/common.ftl" as c>
 
 <@c.page>
-<div>Hello, user</div>
-<a href="/main">Main page</a>
+<h5>Hello, guest</h5>
+<div>This is simple clone of Twitter</div>
+
+
 </@c.page>
